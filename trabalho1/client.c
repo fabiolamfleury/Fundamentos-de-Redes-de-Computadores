c01e06fda87ac6cb012f8523edc7d5e24caf0bf6
fabiolamfleury <fabiola.mfleury@gmail.com>
2018-04-12 00:29:25 -0300

    Adding time out and rtt functions

--- trabalho1/client.c
+++ trabalho1/client.c
@@ -1,64 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //entrada e saída
+#include <stdlib.h> //funções de comparação (string)
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include <sys/time.h>
 
-#define IPADRESS "127.0.0.1"
-#define PORTNUMBER 8888
+#define IPADRESS "127.0.0.1" //localhost: roda em dois terminais no seu proprio computador
+#define PORTNUMBER 8888 //porta escolhido
 #define TRUE 1
-#define SIZE 1024
+#define SIZE 1024 //usa em alocaçõe de memória
 
-int client (char *message){
-  socklen_t addr_size;
-
+struct sockaddr_in create_socket_addr(){
   // configuring server information so that server socket is found
+  //struct: variável conjunta; info necessarias de conexao
   struct sockaddr_in server_socket;
   server_socket.sin_addr.s_addr = inet_addr(IPADRESS);
-  server_socket.sin_family = AF_INET;
+  server_socket.sin_family = AF_INET; //Família do endereço (serve pra UDP e TCP)
   server_socket.sin_port = htons(PORTNUMBER);
-  memset(server_socket.sin_zero, '\0', sizeof server_socket.sin_zero);
+  memset(server_socket.sin_zero, '\0', sizeof server_socket.sin_zero); //memset: jeito mais seguro de utilizar uma string
+  return server_socket;
+}
 
+int send_message(char *message, struct sockaddr_in server_socket){
   // creating socket and sending message through it
-  int sent_bytes = strlen(message) + 1;
-  int client_socket = socket(PF_INET, SOCK_DGRAM, 0);
-  addr_size = sizeof server_socket;
-  sendto(client_socket,message,SIZE,0,(struct sockaddr *)&server_socket,addr_size);
+  int sent_bytes = strlen(message) + 1; //+1 terminador do char
+  int client_socket = socket(PF_INET, SOCK_DGRAM, 0); //DEFINIÇÃO DE TRANSPORTE UDP; abertura do SOCKET no int; PF_INET: Limitador do UDP; SOCK_DGRAM: datagrama->UDP
+  socklen_t addr_size;
+  addr_size = sizeof server_socket;//tamanho do endereço q esta sendo enviado
+  sendto(client_socket,message,SIZE,0,(struct sockaddr *)&server_socket,addr_size); //envio da mensagem; pra quem esta sendo enviando (server_socket) e o tamanho da mensagem
 
+  return client_socket;
+}
+
+int receive_answer (int client_socket, struct sockaddr_in server_socket){ //recebe uma char
   // waiting answer, when receiving it, returning 0
-  while(TRUE){
-    // TODO: Implement time out (udp messages can be lost), return -1
-    char *received_message = (char*)malloc(SIZE * sizeof(char));;
-    int udp_return = recv(client_socket,received_message, SIZE, 0);
-    if (udp_return > 0){
-      printf("recebeu");
-      char *subbuff = (char*)malloc(SIZE - 1 * sizeof(char));
-      memcpy( subbuff, &received_message[0], SIZE -1 );
-      subbuff[udp_return] = '\0';
-      struct timeval tv;
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
 
-      printf("Resposta do servidor %d: %s\n",udp_return,subbuff);
+    char *received_message = (char*)malloc(SIZE * sizeof(char));; //variavel para guardar o q sera recebido do servidor; malloc: separa o espaço do vetor
+    socklen_t addr_size = sizeof server_socket;//tamanho do endereço q esta sendo enviado
+    int udp_return = recvfrom(client_socket,received_message, SIZE, 0, (struct sockaddr *)&server_socket, &addr_size); //tamanho da variavel recebido
+    if (udp_return > 0){ //condição para sair do while
+      //printf("recebeu");
+      char *subbuff = (char*)malloc(SIZE - 1 * sizeof(char)); //variavel auxiliar
+      memcpy( subbuff, &received_message[0], SIZE -1 ); //copia o q esta na mensagem recebida para essa nova variavel
+      subbuff[udp_return] = '\0'; //acrescenta o /0 no final para garantir o printf
+
+   //   printf("Resposta do servidor %d: %s\n",udp_return,subbuff);
+      printf("Resposta do servidor: %s\n\n",subbuff);
+
       return 0;
     }
+
+}
+
+int rsv(char* message){
+
+  struct sockaddr_in server_socket = create_socket_addr();
+
+  int client_socket = send_message(message, server_socket);
+
+  receive_answer(client_socket, server_socket);
+}
+
+int rtt(){
+  char message[SIZE];
+  //do stuff
+  snprintf(message, 4, "rtt \n"); //4: Tamanho da string
+  struct sockaddr_in server_socket = create_socket_addr();
+
+  int client_socket = send_message(message, server_socket);
+  clock_t tInicio = clock();
+
+  int received = receive_answer(client_socket, server_socket);
+  if (received == 0){
+    clock_t tFim = clock();
+
+    double tDecorrido = ((double)(tFim - tInicio)/(CLOCKS_PER_SEC));
+    printf("rtt = %lf segundo\n", tDecorrido);
+  }else{
+    printf("Time out!");
   }
 }
 
-int main(int argc, char const *argv[]){
-  char message[SIZE];
 
-  if (argc > 1){
-     if (strcmp(argv[1], "rsv") == 0 && argc == 3){
+int main(int argc, char const *argv[]){ //argc: quantidade de argumentos argv: conjunto de argumentos
+
+  if (argc > 1){ //a pessoa esta passando so o executavel, nao pode fazer isso
+     if (strcmp(argv[1], "rsv") == 0 && argc == 3){ //CASO DO RSV tem que ter mais um argumento do [nome]
        // if is rsv, send name that will be resolved
-       snprintf(message, SIZE, "rsv: %s", argv[2]);
-       client(message);
+       char message[SIZE];
+       snprintf(message, SIZE, "rsv: %s", argv[2]); //passa pra variavel mensagem para poder manipular
+       rsv(message);
      }
+
      else if (strcmp(argv[1], "rtt") == 0){
        // if is rrt, send rtt
-       snprintf(message, 4, "%s", argv[1]);
-       client(message);
+       int counter;
+       for (counter=0; counter<=9; counter++){ //manda 10x
+         rtt();
+         printf("\n");
+       }
      }
+
      else{
        printf("Argumentos inválidos.\nA função deve ser chamada da seguinte maneira:\n ./client rsv [nome] OU ./client rtt\n");
      }
--- trabalho1/server.c
+++ trabalho1/server.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <netdb.h>
+#include <netdb.h> //prove o serviço do DNS
+#include <time.h>
 
 #define IPADRESS "127.0.0.1"
 #define PORTNUMBER 8888
@@ -14,20 +15,19 @@
 
 int hostname_to_ip(char * hostname , char* ip)
 {
-
-    struct in_addr **addr_list;
-    int i;
     printf("Hostname: %s\n",hostname);
-    struct hostent *hostent = gethostbyname(hostname);
-    if ( hostent == NULL)
+    struct hostent *hostent = gethostbyname(hostname); // STRUCT HOSTENT: //funciona DNS da biblioteca 
+    if ( hostent == NULL) //se IP=0.0.0.0 NAO FOI ENCOTRADO
     {
-        herror("gethostbyname");
+        herror("gethostbyname");  //MUDAR DE ACORDO COM REQUISIÇÃO DO TRABALHO
         return 1;
     }
 
+    struct in_addr **addr_list;
     addr_list = (struct in_addr **) hostent->h_addr_list;
 
-    for(i = 0; addr_list[i] != NULL; i++)
+    int i;
+    for(i = 0; addr_list[i] != NULL; i++) //copia para o IP 
     {
         //Return the first one;
         strcpy(ip , inet_ntoa(*addr_list[i]) );
@@ -39,48 +39,58 @@ int hostname_to_ip(char * hostname , char* ip)
 
 const char* rsv(char *arguments, char *ip){
   int length = strlen(arguments) - 5;
-  char hostname[length + 1];
-  memcpy( hostname, &arguments[5], length);
-  hostname[length] = '\0';
-  hostname_to_ip(hostname , ip);
+  char hostname[length + 1]; //+ é do /0
+  memcpy( hostname, &arguments[5], length); //copiado o nome que se quer resolver
+  hostname[length] = '\0'; //coloca o /0
+  hostname_to_ip(hostname , ip); //envia para a outra função
 
   return ip;
 }
 
 int server (){
-  struct sockaddr_storage server_storage;
-  socklen_t socket_size, client_socket_size;
-
 
   // configuring server address information so that server socket is created properly
+  //Analogo ao cliente
   struct sockaddr_in server_socket_addr;
   server_socket_addr.sin_family = AF_INET;
   server_socket_addr.sin_port = htons(PORTNUMBER);
   server_socket_addr.sin_addr.s_addr = inet_addr(IPADRESS);
   memset(server_socket_addr.sin_zero, '\0', sizeof server_socket_addr.sin_zero);
 
-  int server_socket = socket(PF_INET, SOCK_DGRAM, 0);
+  int server_socket = socket(PF_INET, SOCK_DGRAM, 0); //abre o socket UDP
 
   // binding server socket address with socket created
-  bind(server_socket, (struct sockaddr *) &server_socket_addr, sizeof(server_socket_addr));
+  bind(server_socket, (struct sockaddr *) &server_socket_addr, sizeof(server_socket_addr)); //liga o endereço do socket com o socket criado
 
   /*Initialize size variable to be used later on*/
+  struct sockaddr_storage server_storage; //Guarda as info do cliente para poder enviar as respostas
+  socklen_t socket_size, client_socket_size;
   socket_size = sizeof server_storage;
 
-  while(TRUE){
-    char *buffer = (char*)malloc(SIZE * sizeof(char));
+  while(TRUE){ //sempre funciona
+    char *buffer = (char*)malloc(SIZE * sizeof(char)); //aqui fica a mensagem do cliente
     // will receive any UDP package, storing client information in server_storage so that the properly answer can be send
-    int udp_return = recvfrom(server_socket,buffer,SIZE,0,(struct sockaddr *)&server_storage, &socket_size);
+    int udp_return = recvfrom(server_socket,buffer,SIZE,0,(struct sockaddr *)&server_storage, &socket_size); //receive from 
 
-    char *answer = (char*)malloc(SIZE * sizeof(char));
+    char *answer = (char*)malloc(SIZE * sizeof(char)); //variavel que pega a requisição do buffer e gera uma resposta
+    
+    char msg[5]="Rrtt";
+
+    struct tm time_now= {0};
+    time_t time_clock;
+
+    time(&time_clock);
+    time_now = *localtime(&time_clock);
+
     printf("%s\n", buffer);
-     if (strcmp(buffer, "rtt") == 0){
-        snprintf(answer, SIZE, "Calcular o : %s", buffer);
+     if (strcmp(buffer, "rtt") == 0){ //Calcular o RTT
+      // snprintf(answer, SIZE, "%s Horário da máquina: %d:%d:%d\n", buffer, time_now.tm_hour, time_now.tm_min, time_now.tm_sec);
+ 	snprintf(answer, SIZE, "%s! Horário da máquina do servidor: %d:%d:%d\n", msg, time_now.tm_hour, time_now.tm_min, time_now.tm_sec);
         printf("%d %s\n",udp_return,answer);
 
      }
      else {
-       char ip[100];
+       char ip[100]; //guarda o Ip que sera descoberto
        rsv(buffer, ip);
 
        snprintf(answer, SIZE, "Rrsv: %s", ip);
